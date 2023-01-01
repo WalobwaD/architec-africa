2d4bf089cebf55d7f430232359716a77dbcbc939
WalobwaD <walobwadan@gmail.com>
2023-01-01 13:51:35 +0300

    Pictures

--- components/Pictures.jsx
+++ components/Pictures.jsx
@@ -8,100 +8,29 @@ const Pictures = ()=> {
                 <div className={picStyles.content}>
                     <h1 className={picStyles.title}>Pictures</h1>
                     <hr className={picStyles.hr}></hr>
-                    {/* <div className={picStyles.cards}>
+                    <div className={picStyles.cards}>
                         <div className={picStyles.card}>
                             Picture
                         </div>
-                    </div> */}
-                </div>
-
-            </div>
-            {/* <section className="text-gray-400 bg-black body-font">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-wrap -m-4">
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">The Catalyzer</h2>
-                                <p className="mt-1">Proj1</p>
-                            </div>
+                        <div className={picStyles.card}>
+                            Picture
                         </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261"/>
-                            </a>
-                            <div className="mt-4">
-                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 className="text-white title-font text-lg font-medium">Shooting Stars</h2>
-                            <p className="mt-1">Proj2</p>
-                            </div>
+                        <div className={picStyles.card}>
+                            Picture
                         </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">Neptune</h2>
-                                <p className="mt-1">Proj3</p>
-                            </div>
+                        <div className={picStyles.card}>
+                            Picture
                         </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">The 400 Blows</h2>
-                                <p className="mt-1">Proj4</p>
-                            </div>
+                        <div className={picStyles.card}>
+                            Picture
                         </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">The Catalyzer</h2>
-                                <p className="mt-1">Proj5</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">Shooting Stars</h2>
-                                <p className="mt-1">Proj6</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">Neptune</h2>
-                                <p className="mt-1">Proj7</p>
-                            </div>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <a className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268"/>
-                            </a>
-                            <div className="mt-4">
-                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <h2 className="text-white title-font text-lg font-medium">The 400 Blows</h2>
-                                <p className="mt-1">Proj8</p>
-                            </div>
+                        <div className={picStyles.card}>
+                            Picture
                         </div>
                     </div>
                 </div>
-            </section> */}
+
+            </div>
         </section>
 
     )
