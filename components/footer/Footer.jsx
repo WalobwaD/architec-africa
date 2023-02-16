5a849c50dd75238ba2d433fe5b4ff5f5089609cd
WalobwaD <walobwadan@gmail.com>
2023-02-16 12:43:57 +0300

    Footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,27 +1,45 @@
 import styles from "../../styles/HomePage/Footer/FootStyles.module.scss" 
 import Logo from "../../public/archtivate.png"
 import Image from "next/image"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faInstagram, faLinkedin} from "@fortawesome/free-brands-svg-icons"
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+
 const Footer = ({motion})=> {
     return (
         <section className={styles.footSec} id="contacts">
             <div className={styles.flexContainer}>
                 <div className={styles.content}>
                     <div className={styles.contacts1}>
-                        contacts
+                        <p>Work at archtivate africa</p>
+                        <p>Terms of use</p>
+                        <p>Privacy policy</p>
+                        <p>Cookie policy</p>
+                        <p>RSS</p>
                     </div>
-                    <Image 
-                        className={styles.logo}
-                        src={Logo}
-                        alt="Logo images"
-                    />
+
                     <div className={styles.icons}>
-                        <FontAwesomeIcon icon={faLinkedin} />
-                        <FontAwesomeIcon icon={faInstagram} />
+                        <Image 
+                            className={styles.logo}
+                            src={Logo}
+                            alt="Logo images"
+                        />
+                        <div className={styles.social}>
+                            <FacebookIcon />
+                            <YouTubeIcon />
+                            <TwitterIcon />
+                            <PinterestIcon />
+                        </div>
+
                     </div>
                     <div className={styles.contacts2}>
-                        contacts
+                        <h3>Connect with Archtivate Africa</h3>
+                        <p>About archtivate africa</p>
+                        <p>Connect with the editors</p>
+                        <p>NewsLetter sign up</p>
+                        <p>Contact advertising</p>
                     </div>
                 </div>
             </div>
