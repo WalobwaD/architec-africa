36c09f051f9c65f33a98da29f90b9fd646fc53a7
Walobwa_D <walobwadan@gmail.com>
2023-03-10 09:27:08 +0300

    POST AND WEB

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
+import styles from "../styles/PostsPage/index.module.scss"
 const posts = () => {
     return (
-        <div>
-            <Link href="/">Back</Link>
+        <div className={styles.container}>
             <div>ARCHTIVATE POSTS</div>
+            <h2>COMING SOON</h2>
+            <Link href="/">Back</Link>
+
         </div>
     )
 }
--- pages/web.jsx
+++ pages/web.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link"
+import styles from "../styles/WebPage/index.module.scss"
 const web = () => {
     return (
-        <div>
-            <Link href="/">Back</Link>
+        <div className={styles.container}>
 
             <div>ARCHTIVATE WEB</div>
+            <h2>COMING SOON</h2>
+            <Link href="/">Back</Link>
+
         </div>
     )
 }
